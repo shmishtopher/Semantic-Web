7f773c2fc1ac44d78b135c5defad25bbee5b8d1f
Shmish <shmish90@gmail.com>
2021-05-02 20:02:10 -0400

    Added ontology

--- Final_Project/src/App.java
+++ Final_Project/src/App.java
@@ -1,5 +1,19 @@
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        // Define an empty RDF model and open a stream to
+        // the data source.
+        Model schema = ModelFactory.createDefaultModel();
+        InputStream stream = new URL("https", "raw.githubusercontent.com", "/shmishtopher/Semantic-Web/master/Final_Project/cars.owl").openStream();
 
+        // Stream the remote data source into the default
+        // model and clean up the stream.
+        schema.read(stream, null, "TURTLE");
+        stream.close();
     }
 }
